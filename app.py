234f1d1c16710d21fba5914f981f3d69c54ec4c4
chrisyeh09111 <65944341+chrisyeh09111@users.noreply.github.com>
2020-05-26 16:14:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-import os
-
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -7,23 +5,24 @@ import dash_html_components as html
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
 server = app.server
+app.title=tabtitle
+
 
 app.layout = html.Div([
-    html.H2('Hello World'),
-    dcc.Dropdown(
-        id='dropdown',
-        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
-        value='LA'
-    ),
-    html.Div(id='display-value')
+    dcc.Input(id='my-id', value='initial value', type='text'),
+    html.Div(id='my-div')
 ])
 
-@app.callback(dash.dependencies.Output('display-value', 'children'),
-              [dash.dependencies.Input('dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
+
+@app.callback(
+    dash.dependencies.Output(component_id='my-div', component_property='children'),
+    [dash.dependencies.Input(component_id='my-id', component_property='value')]
+)
+def update_output_div(input_value):
+    return 'You\'ve entered "{}"'.format(input_value)
+
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server()    
+    
