6e26aa77777582583f8238a15a64a91e4f654f4c
chrisyeh09111 <65944341+chrisyeh09111@users.noreply.github.com>
2020-06-01 18:27:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,7 @@ import json
 import os
 import datetime
 import pandas as pd
-import boto3
-# dbi = dynamo.DBInterface(profile_name='prod', n_processes=8)
 
-# # ss = dbi.students.all('phone,name,aggregates,class_code,student_pin,teacher,schoolCode')
-# user_store = dbi.classes.get_by_school(
-#             school_code='ZGOBJP',
-#             projection='code,schoolCode,aggregates')
-# os.chdir('C:/Users/chris/Desktop/studycat')
-# with open('Copy of rise_2020-04-08.json') as f:
-#     data = json.loads(f.read())
-# data=data[0]
 df = pd.read_csv('https://raw.githubusercontent.com/chrisyeh09111/flying-dog-beers/master/hello.csv')
 df['aggregates']=df['aggregates'].apply(lambda x:json.loads(x.replace("\'","\"")))
 import dash_core_components as dcc
@@ -30,42 +20,38 @@ app.layout = html.Div([
         dcc.Tab(label='Weekly', value='weekly'),
         dcc.Tab(label='Monthly', value='monthly'),        
     ]),
-    
-    
-    
+
+
+
     dcc.Dropdown(
                 id='yaxis-column',
-                options=[{'label': i, 'value': i} for i in available_indicators],
-                value='GN_L3_B'
+                options=[{'label': i, 'value': i} for i in available_indicators],                value='GN_L3_B'
             ),dcc.RadioItems(
-                id='xaxis-column',
-                options=[{'label': 'Progress Through Curriculum', 'value': 'p'},{'label': 'Average Score', 'value': 'as'}
-                         ,{'label': 'Number of Plays', 'value': 'ac'},{'label': 'Average Play Time (secs)', 'value': 'ad'}
-                        ,{'label': 'Average Student Play Time (secs)', 'value': 'asd'}, {'label': 'Total Play Time (secs)', 'value': 'd'}],
+                id='xaxis-column',  options=[{'label': i, 'value': i} for i in available_indicators1],
                 value='as'
             )
-                       
-                       
-                       
-                       
-                       
+
+
+
+
+
                        ,
                 dcc.Graph(id='indicator-graphic')
-    
-            
-    
+
+
+
 ])
 @app.callback(
     dash.dependencies.Output('indicator-graphic', 'figure'),
     [
      dash.dependencies.Input('yaxis-column', 'value'),
-     dash.dependencies.Input('xaxis-column', 'value'),   
+  dash.dependencies.Input('xaxis-column', 'value'),   
      dash.dependencies.Input('tabs-example', 'value')
     ])
 def update_graph(yaxis_column, xaxis_column, tabs_exp):
 
     df1 = df[df['name']==yaxis_column]
-    
+
     df1 = df1.reset_index()['aggregates'][0][tabs_exp]
     df1=pd.DataFrame(df1)
     if tabs_exp=='monthly':
@@ -79,11 +65,7 @@ def update_graph(yaxis_column, xaxis_column, tabs_exp):
         'data': [
                {'x': df1[temp], 'y':df1[xaxis_column], 'type': 'bar', 'name': 'SF'},
             ],
-        'layout': {'title': 'Dash Data Visualization'
-                   
-                   
-             }}
+        'layout': {'title': 'Dash Data Visualization'    }}
             
-
 if __name__ == '__main__':
     app.run_server()
